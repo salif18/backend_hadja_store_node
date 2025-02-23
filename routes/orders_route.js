1ea1e7c452e231bbc594650ed90098cf50f18898
salif18 <salifmoctarkonate@gmail.com>
2025-02-23 12:36:05 +0000

    correction de route

--- routes/orders_route.js
+++ routes/orders_route.js
@@ -5,5 +5,5 @@ const Orders_Controller = require("../controller/order_conroller");
 Router.post("/",Orders_Controller.createOrder);
 Router.get("/",Orders_Controller.getAllOrders)
 Router.get("/status/:status",Orders_Controller.getOrdersByStatus)
-Router.get("/status/:userId",Orders_Controller.getOrdersByUser)
+Router.get("/:userId",Orders_Controller.getOrdersByUser)
 module.exports = Router;
