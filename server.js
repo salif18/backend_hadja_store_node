4409ec1b62b018daf5fb7a99da890edfda1c2525
salif18 <salifmoctarkonate@gmail.com>
2025-03-01 18:27:21 +0000

    enleve socket

--- server.js
+++ server.js
@@ -4,47 +4,47 @@ const app = require("./app");
 const http = require("http");
 // Créer le serveur
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 
 
 app.set('port',process.env.PORT || 8080 );
 
 
 // Configurer Socket.IO
-const io = new Server(server, {
-  cors: {
-    origin: "*", // À restreindre en production
-    methods: ["GET","POST", "PUT", "DELETE", "PATCH","OPTIONS"],
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*", // À restreindre en production
+//     methods: ["GET","POST", "PUT", "DELETE", "PATCH","OPTIONS"],
+//   },
+// });
 
-// Gérer les connexions WebSocket
-io.on('connection', (socket) => {
-  console.log(`Client connecté: ${socket.id}`);
+// // Gérer les connexions WebSocket
+// io.on('connection', (socket) => {
+//   console.log(`Client connecté: ${socket.id}`);
 
-  // Écouter l'événement de connexion de l'utilisateur
-  socket.on('join-room', (userId) => {
-    socket.join(userId);
-    console.log(`Utilisateur ${userId} a rejoint sa salle`);
-  });
+//   // Écouter l'événement de connexion de l'utilisateur
+//   socket.on('join-room', (userId) => {
+//     socket.join(userId);
+//     console.log(`Utilisateur ${userId} a rejoint sa salle`);
+//   });
 
-  // Écouter l'événement de sélection de livreur
-  socket.on('post-livreur', (data) => {
-    console.log('Événement livreur-selectionne reçu:', data);
-    io.to(data.userId).emit('get-notification', {
-      userId: data.userId,
-      orderId: data.orderId,
-      username:data.username,
-      message: data.message,
-      createdAt: new Date(),
-    });
-  });
+//   // Écouter l'événement de sélection de livreur
+//   socket.on('post-livreur', (data) => {
+//     console.log('Événement livreur-selectionne reçu:', data);
+//     io.to(data.userId).emit('get-notification', {
+//       userId: data.userId,
+//       orderId: data.orderId,
+//       username:data.username,
+//       message: data.message,
+//       createdAt: new Date(),
+//     });
+//   });
 
-  // Écouter l'événement de déconnexion
-  socket.on('disconnect', () => {
-    console.log(`Client déconnecté: ${socket.id}`);
-  });
-});
+//   // Écouter l'événement de déconnexion
+//   socket.on('disconnect', () => {
+//     console.log(`Client déconnecté: ${socket.id}`);
+//   });
+// });
 
 // Démarrer le serveur
 server.listen(process.env.PORT || 8080, () => {
@@ -54,6 +54,6 @@ server.listen(process.env.PORT || 8080, () => {
 // Gestion des erreurs du serveur
 server.on('error', (error) => {
   console.error('Erreur du serveur:', error);
-});;
+});
 
 
