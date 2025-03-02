e96fc8ca6b54da98878895ad08693891e30eadca
salif18 <salifmoctarkonate@gmail.com>
2025-03-02 05:33:24 +0000

    corre

--- controller/notification_controller.js
+++ controller/notification_controller.js
@@ -26,13 +26,13 @@ exports.sendNotification = async (req, res) => {
         title: req.body.title,
         body:req.body.message, 
       },
-      data:{
-        orderId: new mongoose.Types.ObjectId(req.body.orderId),
-        username:user.name,
-        message: req.body.message,
-        read:false,
-        createdAt: new Date()
-      }
+      // data:{
+      //   orderId: new mongoose.Types.ObjectId(req.body.orderId),
+      //   username:user.name,
+      //   message: req.body.message,
+      //   read:false,
+      //   createdAt: new Date()
+      // }
     });
     
     return res.status(201).json(notification);
